24c469ca5cc00c077d43829af8cd07a9eae4510f
Nishant Rai <nishant.rai@comprotechnologies.com>
2022-07-28 13:56:43 +0530

    Book Catalog: fixed class methods declaration and changed to single quotes

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 class Books {
   constructor() {
     this.sortCheckFlag = 1;
-
     this.bookList = [];
   }
 
-  getBooks = async () => {
+  async getBooks() {
     try {
       const res = await fetch("data/books.json");
       const fetchedBooks = await res.json();
@@ -14,9 +13,9 @@ class Books {
     } catch (error) {
       console.log(error);
     }
-  };
+  }
 
-  searchById = () => {
+  searchById() {
     const id = Number(document.querySelector(".js-input-search-id").value);
     if (id) {
       const book = this.bookList.find((book) => book.bookId === id);
@@ -28,9 +27,9 @@ class Books {
     } else {
       alert("Please Enter valid Book id!");
     }
-  };
+  }
 
-  searchByGenre = () => {
+  searchByGenre() {
     const bookGenre = document.querySelector(".js-input-search-genre").value;
     if (bookGenre) {
       const similarBooksByGenre = this.bookList.filter((book) =>
@@ -40,37 +39,37 @@ class Books {
     } else {
       alert("Please Enter valid input!");
     }
-  };
+  }
 
-  searchByPrice = () => {
+  searchByPrice() {
     const price = Number(
       document.querySelector(".js-input-search-price").value
     );
     if (price) {
       const similarBooksByPrice = this.bookList.filter(
-        (book) => Math.abs(book.price - price) < 2
+        (book) => Math.abs(book.price - price) < 10
       );
       insertToTable(similarBooksByPrice, tableSimilarBooks);
     } else {
       alert("Please Enter valid input!");
     }
-  };
+  }
 
-  examineBook = (bookId) => {
+  examineBook(bookId) {
     const book = this.bookList.find((book) => book.bookId === bookId);
     const list = document.querySelector(".js-list-examined-book");
     list.querySelector(".js-li-bookid").innerHTML = book.bookId;
     list.querySelector(".js-li-price").innerHTML = book.price;
     list.querySelector(".js-li-genre").innerHTML = book.genre;
-  };
+  }
 
-  sortByPrice = () => {
+  sortByPrice() {
     // for ascending/descending
     this.sortCheckFlag = -this.sortCheckFlag;
     const booksCopy = [...this.bookList];
     booksCopy.sort((a, b) => this.sortCheckFlag * (a.price - b.price));
     insertToTable(booksCopy, tableAllBooks);
-  };
+  }
 }
 
 const insertToTable = (booksArray, table) => {
@@ -82,7 +81,7 @@ const insertToTable = (booksArray, table) => {
     row.insertCell(2).innerHTML = book.price;
     row.insertCell(
       3
-    ).innerHTML = `<button class="btn" onclick="books.examineBook(${book.bookId})">Examine</button>`;
+    ).innerHTML = `<button class='btn' onclick='books.examineBook(${book.bookId})'>Examine</button>`;
   });
 };
 
@@ -101,13 +100,13 @@ books.getBooks();
 
 document
   .querySelector(".js-btn-search-id")
-  .addEventListener("click", books.searchById);
+  .addEventListener("click", books.searchById.bind(books));
 document
   .querySelector(".js-btn-search-genre")
-  .addEventListener("click", books.searchByGenre);
+  .addEventListener("click", books.searchByGenre.bind(books));
 document
   .querySelector(".js-btn-search-price")
-  .addEventListener("click", books.searchByPrice);
+  .addEventListener("click", books.searchByPrice.bind(books));
 document
   .querySelector(".js-sort-table-price")
-  .addEventListener("click", books.sortByPrice);
+  .addEventListener("click", books.sortByPrice.bind(books));
